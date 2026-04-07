7a076fdb18f4bff73ce8048693fb6e21759ac51a
Mustafa Guven, Vodafone <Mustafa.Guven@vodafone.com>
2026-04-07 13:46:35 +0300

    feat: init flutter project

---
+++ android/app/src/main/kotlin/com/mustafaguven/samsung_kumanda/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mustafaguven.samsung_kumanda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Samsung Remote',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1565C0),
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(body: Center(child: Text('Samsung Remote'))),
+    );
+  }
+}
